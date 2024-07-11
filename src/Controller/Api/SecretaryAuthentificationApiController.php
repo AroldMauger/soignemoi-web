a5ed3478bf391ef3ae4c23f629bb3519e8d9839e
Arold Mauger <aroldmauger2022@gmail.com>
2024-07-11 15:50:10 +0200

    Génération d'un token pour l'authentification des secrétaires

--- src/Controller/Api/SecretaryAuthentificationApiController.php
+++ src/Controller/Api/SecretaryAuthentificationApiController.php
@@ -12,30 +12,28 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Csrf\TokenGenerator\TokenGeneratorInterface;
 
 class SecretaryAuthentificationApiController extends AbstractController
 {
     private $entityManager;
     private $passwordHasher;
+    private $tokenGenerator;
 
-    public function __construct(EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher)
+    public function __construct(EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher, TokenGeneratorInterface $tokenGenerator)
     {
         $this->entityManager = $entityManager;
         $this->passwordHasher = $passwordHasher;
+        $this->tokenGenerator = $tokenGenerator;
     }
 
     #[Route('/api/secretary/login', name: 'api_secretary_login', methods: ['POST'])]
     public function login(Request $request): Response
     {
-        // Log the raw content of the request
         $rawContent = $request->getContent();
-        file_put_contents('php://stderr', print_r($rawContent, true));
-
         $data = json_decode($rawContent, true);
 
         if (json_last_error() !== JSON_ERROR_NONE) {
-            // Log JSON decode error
-            file_put_contents('php://stderr', print_r(json_last_error_msg(), true));
             return new JsonResponse(['message' => 'Invalid JSON'], Response::HTTP_BAD_REQUEST);
         }
 
@@ -56,6 +54,11 @@ class SecretaryAuthentificationApiController extends AbstractController
             return new JsonResponse(['message' => 'Invalid credentials'], Response::HTTP_UNAUTHORIZED);
         }
 
-        return new JsonResponse(['message' => 'Success'], Response::HTTP_OK);
+        $csrfToken = $this->tokenGenerator->generateToken();
+
+        return new JsonResponse([
+            'message' => 'Success',
+            'csrf_token' => $csrfToken
+        ], Response::HTTP_OK);
     }
 }
